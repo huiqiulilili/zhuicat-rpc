e57e100824320e963ed4e27ae98a4c9061a0f310
huiqiulilili <2825084776@qq.com>
2024-05-22 17:38:13 +0800

    实现用户可以 自定义序列化器 （利用SPI机制）

--- zhuicat-rpc-core/src/main/java/com/zhuicat/zhuirpc/serializer/SerializerFactory.java
+++ zhuicat-rpc-core/src/main/java/com/zhuicat/zhuirpc/serializer/SerializerFactory.java
@@ -1,5 +1,7 @@
 package com.zhuicat.zhuirpc.serializer;
 
+import com.zhuicat.zhuirpc.spi.SpiLoader;
+
 import java.util.Map;
 
 /**
@@ -9,26 +11,43 @@ import java.util.Map;
  * @since 2024/5/22 15:01
  */
 public class SerializerFactory {
-    /**
-     * 序列化映射（用于实现单例）
-     */
-    private static final Map<String, Serializer> KEY_SERIALIZER_MAP = Map.of(
-            SerializerKeys.JDK, new JdkSerializer(),
-            SerializerKeys.JSON, new JsonSerializer(),
-            SerializerKeys.KRYO, new KryoSerializer(),
-            SerializerKeys.HESSIAN, new HessianSerializer()
-    );
+
+    static {
+        SpiLoader.load(Serializer.class);
+    }
 
     /**
      * 默认序列化器
      */
-    private static final Serializer DEFAULT_SERIALIZER = KEY_SERIALIZER_MAP.get(SerializerKeys.JDK);
+    private static final Serializer DEFAULT_SERIALIZER = new JdkSerializer();
 
     /**
      * 获取实例
      */
     public static Serializer getInstance(String key) {
-        return KEY_SERIALIZER_MAP.getOrDefault(key, DEFAULT_SERIALIZER);
+        return SpiLoader.getInstance(Serializer.class, key);
     }
+
+//    /**
+//     * 序列化映射（用于实现单例）
+//     */
+//    private static final Map<String, Serializer> KEY_SERIALIZER_MAP = Map.of(
+//            SerializerKeys.JDK, new JdkSerializer(),
+//            SerializerKeys.JSON, new JsonSerializer(),
+//            SerializerKeys.KRYO, new KryoSerializer(),
+//            SerializerKeys.HESSIAN, new HessianSerializer()
+//    );
+//
+//    /**
+//     * 默认序列化器
+//     */
+//    private static final Serializer DEFAULT_SERIALIZER = KEY_SERIALIZER_MAP.get(SerializerKeys.JDK);
+//
+//    /**
+//     * 获取实例
+//     */
+//    public static Serializer getInstance(String key) {
+//        return KEY_SERIALIZER_MAP.getOrDefault(key, DEFAULT_SERIALIZER);
+//    }
 
 }
